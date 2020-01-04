0cfb8c9becc32620d4e97f41a0cb474c85dca045
Raghunandan.Seshadri <raghubs81@gmail.com>
2020-01-04 14:42:18 +0530

    First commit - MultiContainer

---
+++ web-server/src/App.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Section emptied to avoid crash -- App component attempts to Fib with attempts to access React server (not running)
+it('renders without crashing', () => {
+});
